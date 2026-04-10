419c6e42e0826a11455064611690e647776256fb
MuneebM07 <muneeb.dev01@gmail.com>
2026-04-10 23:18:28 +0530

    index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import dotenv from "dotenv"
+import express from "express";
 
+import dotenv from "dotenv"
+import router from "./routes/schedule.route.js";
+
+const app = express();
+
+app.use(express.json())
 dotenv.config();
+
+app.use("/api",router)
+
+app.listen(3000,()=>{
+    console.log("server connected ");
+})
