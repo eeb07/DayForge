eac7cf39bea2451e4ef484a7c19d16c0800b2a05
MuneebM07 <muneeb.dev01@gmail.com>
2026-05-02 16:54:33 +0530

    feat: ocr

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv"
 import router from "./routes/schedule.route.js";
 import authrouter from "./routes/auth.route.js";
+import { ocrController } from "./controllers/ocr.controller.js";
+import ocrRouter from "./routes/ocr.route.js";
 
 const app = express();
 
@@ -11,6 +13,7 @@ dotenv.config();
 
 app.use("/api",router)
 app.use("/auth", authrouter)
+app.use("/api",ocrRouter)
 
 app.listen(5000,()=>{
     console.log("server connected ");
